06ac01fe55514ba08bb0f6ca60578ee933dd4722
SonJava10Rikkei <sonjapan7@gmail.com>
2022-12-04 13:22:38 +0700

    a

---
+++ F8/JavaScript-Co-Ban/2.10_Cu_pháp_comments/main.js
@@ -0,0 +1,28 @@
+/**
+ * Comments trong javascript
+ * 1. Muc Dich Su Dung
+ * - Ghi chú
+ * - Vô hiệu hóa mã
+ * 
+ * 
+ * 2. Loại comments
+- Comment 1 dòng
+- Comment nhiều dòng
+
+3. Sử dụng phím tắt
+- Window: Ctrl + /
+- MacOs: Command + /
+
+*/
+
+/**
+ * - Dung dau gach / va 2 dau ** 
+ */
+
+
+// Khai bao bien
+var fullName = 'Nguyen Danh Son';
+var age = 27; // Khai bao tuoi
+// Goi ham thong bao
+//alert(fullName);
+alert(age);
---
+++ F8/JavaScript-Co-Ban/2.8.Khai_niem_Bien_va_cach_su_dung/main.js
@@ -0,0 +1,7 @@
+// Tao 1 ten bien la fullName va gan gia tri cho bien la Nguyen Danh Son
+var fullName = 'Nguyen Danh Son';
+var age = 26;
+
+alert(fullName) // Hien thi gia tri cua bien tren o popup
+alert(age)
+
